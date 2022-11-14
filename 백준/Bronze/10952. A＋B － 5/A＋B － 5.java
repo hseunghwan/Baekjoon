9c8eb950106d5d84a1b542f4960ddda426143729
hyunsh <47944635+hseunghwan@users.noreply.github.com>
2022-11-14 21:50:56 +0900

    [Bronze V] Title: A+B - 5, Time: 132 ms, Memory: 14544 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        int a,b;
+         a = scan.nextInt();
+         b = scan.nextInt();
+        while(a!=0||b!=0){
+            System.out.println(a+b);
+            a = scan.nextInt();
+            b = scan.nextInt();
+        }
+    }
+}
