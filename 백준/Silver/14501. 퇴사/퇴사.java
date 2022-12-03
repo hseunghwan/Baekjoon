6cf81c9ec4ca68ae470cadef5b46c1012306cd5a
hyunsh <47944635+hseunghwan@users.noreply.github.com>
2022-12-03 18:30:13 +0900

    [Silver III] Title: 퇴사, Time: 128 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,57 +1,50 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class Main {
-    public static boolean caninsert(boolean table[], int date, int length) {
-        if (!table[date] && (table.length - date >= length)) {
-            for (int i = 0; i < length; i++) {
-                table[date++] = true;
-            }
-            return true;
-        } else {
-            return false;
-        }
-    }
+    // 1. 뒤에서 부터 For문을 돌기
+    // 2. 해당 배열 순서 + 경과 기간 >= N 이면 넘기기
+    // 3. 합의 최댓값 으로 갱신 해두기
+    static class Consult {
+        int time;
+        int profit;
 
-    public static int cal(int[][] input, boolean[] table, int date, boolean tf, int val) {
-        if (date >= table.length) {
-            return val;
+        Consult(int time, int profit) {
+            this.time = time;
+            this.profit = profit;
         }
-        if (tf && caninsert(table, date, input[date][0])) {
-            val += input[date][1];
-            int a = cal(input, table.clone(), date + 1, true, val);
-            int b = cal(input, table.clone(), date + 1, false, val);
-            return a > b ? a : b;
-        } else {
-            int a = cal(input, table.clone(), date + 1, true, val);
-            int b = cal(input, table.clone(), date + 1, false, val);
-            return a > b ? a : b;
-        }
-    }
 
+    }
+    private static int dp[], N;
+    private static Consult[] input;
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 선언
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); // 선언
-        // String s = br.readLine(); //문자열로 받기
-        int N = Integer.parseInt(br.readLine()); // 정수로 받기
-        String[][] temp = new String[N][2];
-        int[][] input = new int[N][2];
+//        Scanner br = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        dp= new int[N];
+
+        input = new Consult[N];
+
         for (int i = 0; i < N; i++) {
-            temp[i] = (br.readLine()).split(" ");
-            input[i][0] = Integer.parseInt(temp[i][0]);
-            input[i][1] = Integer.parseInt(temp[i][1]);
+            String temp[] = br.readLine().split(" ");
+            input[i] = new Consult(Integer.parseInt(temp[0]), Integer.parseInt(temp[1]));
+            dp[i] = -1;
         }
 
-        boolean[] table = new boolean[N];
-        int a = cal(input, table.clone(), 0, true, 0);
-        int b = cal(input, table.clone(), 0, false, 0);
-        int val = a > b ? a : b;
+        System.out.print(recur(0));
+    }
 
-        bw.write(val + "");
-        bw.flush();
-        bw.close();
+    private static int recur(int date) {
+        if(date > N) return Integer.MAX_VALUE;
+
+        if(date == N) return 0;
+
+        if(dp[date] != -1) return dp[date];
+
+        dp[date] = Math.max(recur(date + input[date].time) + input[date].profit, recur(date + 1));
+
+        return dp[date];
     }
 }
